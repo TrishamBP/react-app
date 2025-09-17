bfe83f94ec9bff680825633fb7e8f6a7ddd9a419
Trisham Bharat Patil <trishampatil@gmail.com>
2025-09-17 19:02:22 +0530

    Initial Commit.

---
+++ src/components/404.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ fontSize: "72px", marginBottom: "20px" }}>404</h1>
+      <p style={{ fontSize: "24px" }}>Page Not Found</p>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {}, // Changed from extends to extend
+  },
+  plugins: [],
+};
